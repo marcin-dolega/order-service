f2d942b9574c63089c148b3086b84ff1cd3fbc5c
Marcin Dolega <marcin92d@gmail.com>
2022-11-25 12:50:36 +0100

    V1.0 migration corrected

--- src/main/resources/db/migration/V1.0__db_init.sql
+++ src/main/resources/db/migration/V1.0__db_init.sql
@@ -1,7 +1,6 @@
-drop table if exists flyway_schema_history;
 drop table if exists order_service;
 
-create table order_service
+create table order_header
 (
     id        bigint not null auto_increment primary key,
     customer_name      varchar(255)
