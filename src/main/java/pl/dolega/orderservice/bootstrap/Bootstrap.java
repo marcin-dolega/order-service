4c90ccdccc0bee33c87581acafccd1119fd19827
Marcin Dolega <marcin92d@gmail.com>
2022-12-05 16:20:37 +0100

    Bootstrap Class

---
+++ src/main/java/pl/dolega/orderservice/bootstrap/Bootstrap.java
@@ -0,0 +1,14 @@
+package pl.dolega.orderservice.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("I was called");
+    }
+
+}
