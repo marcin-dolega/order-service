e369e7b470c82c7dd9dd98936981f32f48ef570f
Marcin Dolega <marcin92d@gmail.com>
2022-12-06 10:20:00 +0100

    Optimistic locking

--- src/main/java/pl/dolega/orderservice/bootstrap/Bootstrap.java
+++ src/main/java/pl/dolega/orderservice/bootstrap/Bootstrap.java
@@ -3,10 +3,8 @@ package pl.dolega.orderservice.bootstrap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import pl.dolega.orderservice.customer.Customer;
 import pl.dolega.orderservice.customer.CustomerRepository;
-import pl.dolega.orderservice.orderHeader.OrderHeader;
 import pl.dolega.orderservice.orderHeader.OrderHeaderRepository;
 
 @Component
@@ -37,10 +35,20 @@ public class Bootstrap implements CommandLineRunner {
         bootstrapOrderService.readOrderData();
 
         Customer customer = new Customer();
-        customer.setCustomerName("Testing Version");
-        Customer savedCustomer = customerRepository.save(customer);
 
+        customer.setCustomerName("Testing Version 0");
+        Customer savedCustomer = customerRepository.save(customer);
         System.out.println("Version is: " + savedCustomer.getVersion());
+
+        savedCustomer.setCustomerName("Testing Version 1");
+        Customer savedCustomer1 = customerRepository.save(savedCustomer);
+        System.out.println("Version is: " + savedCustomer1.getVersion());
+
+
+        savedCustomer1.setCustomerName("Testing Version 2");
+        Customer savedCustomer2 = customerRepository.save(savedCustomer1);
+        System.out.println("Version is: " + savedCustomer2.getVersion());
+
         customerRepository.deleteById(savedCustomer.getId());
     }
 
